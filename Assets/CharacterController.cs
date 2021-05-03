7a453b080d79a205d07196282e9e0ca6a143221b
glen <glen.piercev@gmail.com>
2021-05-02 19:15:41 -0700

    simple player controls

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+    private string _moveXAxisName;
+    private string _moveYAxisName;
+    private string _lookXAxisName;
+    private string _lookYAxisName;
+    private string _aButtonAxisName;
+    private string _bButtonAxisName;
+    private string _dashButtonAxisName;
+    private int _playerNumber;
+    private Player _player;
+
+    private float _moveX;
+    private float _moveY;
+    private float _lookX;
+    private float _lookY;
+    private float _aButton;
+    private float _bButton;
+    private float _dashButtonInputValue;
+
+    public float _rotationY;
+    public float _rotationX;
+
+    public bool _isDashing;
+    private float _dashFuel = 12;
+    private float _nextInkTime;
+
+    private bool _isSitting;
+
+    public static readonly int MaxDashFuel = 12;
+    public static readonly int DashFuelConsumeRate = 1;
+    public static readonly int DashFuelReplenishRate = 1;
+    public static readonly float InkIntervalSeconds = 0.25f;
+
+    public Vector2 sensitivity = new Vector2(2, 2);
+    public Vector2 smoothing = new Vector2(3, 3);
+
+    Rigidbody myRigidBody;
+    private Animator _animator;
+
+    public void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        myRigidBody = GetComponentInParent<Rigidbody>();
+        myRigidBody.useGravity = true;
+        _player = gameObject.GetComponent<Player>();
+        myRigidBody.drag = _player.drag;
+        _animator = gameObject.GetComponentInChildren<Animator>();
+    }
+
+    public void Update() {
+        myRigidBody.mass = _player.weight;
+    }
+
+    public void FixedUpdate() {
+
+        UpdateInput();
+
+        if (_isDashing) {
+            _player.speed = _player.dashSpeed;
+        } else {
+            _player.speed = _player.normalSpeed;
+        }
+
+
+        var xMovement = _moveX;
+        var zMovement = _moveY;
+        var yMovement = (_aButton) - (_bButton);
+        var movementVector = new Vector3(xMovement, yMovement, zMovement) * _player.speed * 5;
+        myRigidBody.AddRelativeForce(movementVector);
+
+        _isSitting = myRigidBody.velocity.magnitude < 0.1f;
+
+        if(_animator != null) {
+            _animator.SetBool("Sit_b", _isSitting);
+            _animator.SetFloat("Speed_f", movementVector.magnitude);
+            // _animator.SetBool("IsDashing", _isDashing);
+        }
+
+        // _rotationX = _rotationX + (_lookY * _player.turnSpeed);
+        _rotationY = _rotationY + (_lookX * _player.turnSpeed);
+
+        transform.localEulerAngles = new Vector3(_rotationX, _rotationY);
+
+        //Debug.Log(myRigidBody.velocity);
+        //Debug.Log(myRigidBody.velocity.magnitude);
+        
+
+        HandleDash();
+    }
+
+    private void HandleDash() { 
+        // if the button is pressed
+        if (DashButtonPressed()) {
+            // if we have Fuel left
+            if (_dashFuel > 0) {
+                _isDashing = true;
+                _dashFuel -= Time.deltaTime * DashFuelConsumeRate;
+            } else {
+                _isDashing = false;
+                _dashFuel = 0;
+            }
+
+        } else {
+            _isDashing = false;
+            _dashFuel += Time.deltaTime * DashFuelReplenishRate;
+            if (_dashFuel > MaxDashFuel) {
+                _dashFuel = MaxDashFuel;
+            }
+        }
+	}
+
+    private bool DashButtonPressed() {
+        return _dashButtonInputValue > 0;
+    }
+
+    private void UpdateInput() {
+        _moveX = getMoveX(); //Input.GetAxis(_moveXAxisName);
+        _moveY = getMoveY(); //Input.GetAxis(_moveYAxisName);
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity.x * smoothing.x, sensitivity.y * smoothing.y));
+        _lookX = mouseDelta.x; // Input.mousePosition.x; //Input.GetAxis(_lookXAxisName);
+        _lookY = mouseDelta.y; //Input.mousePosition.y; //Input.GetAxis(_lookYAxisName);
+        _aButton = 0; //Input.GetAxis(_aButtonAxisName);
+        _bButton = 0; //Input.GetAxis(_bButtonAxisName);
+        _dashButtonInputValue = 0; //Input.GetAxis(_dashButtonAxisName);
+    }
+
+    private int getMoveX() {
+        if(Input.GetKey(KeyCode.D)) {
+            return 1;
+        } else if(Input.GetKey(KeyCode.A)) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    private int getMoveY() {
+        if(Input.GetKey(KeyCode.W)) {
+            return 1;
+        } else if(Input.GetKey(KeyCode.S)) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public int weight = 50;
+    public int drag = 5;
+    public static float speedNormalizer = 1000.0f;
+    public float speed = 1.0f * speedNormalizer;
+    public float normalSpeed = 1.0f * speedNormalizer;
+    public float dashSpeed = 9f * speedNormalizer;
+    public float turnSpeed = 3f;
+}
