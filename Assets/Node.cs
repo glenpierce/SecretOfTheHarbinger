3c6b6066ea5a505c69f1d014e6febeacd9ee3454
glen <glen.piercev@gmail.com>
2021-07-22 10:33:44 -0700

    initial A* from Sebastian Lague Tutorial

---
+++ Assets/Node.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+
+    public bool myWalkable;
+    public Vector3 myWorldPosition;
+
+    public Node(bool walkable, Vector3 worldPosition) {
+        myWalkable = walkable;
+        myWorldPosition = worldPosition;
+    }
+}
